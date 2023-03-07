0430da300493cf78dacb2df0293c906c1dde5b0a
Kirill Nemtsov <KN17022004@yandex.ru>
2023-03-08 00:28:49 +0500

    array

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace Project_test
     {
         static void Main(string[] args)
         {
+            int[] ints = new int[100];
+            Random rnd = new Random();
+            for (int i = 0; i < ints.Length; i++) 
+            {
+                ints[i] = rnd.Next(0,109099);
+                Console.WriteLine(ints[i]);
+            }
         }
     }
 }
